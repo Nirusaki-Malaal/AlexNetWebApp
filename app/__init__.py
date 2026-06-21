6bb9d3a977d680e461e78d3a2a999a67904d63ae
nirusaki-malaal <mradulkumar07ocb@gmail.com>
2026-06-22 00:08:48 +0530

    perf(webapp): load model globally on startup and share instance for predictions

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
-import os, logging
+import os, logging, torch
 from logging.handlers import RotatingFileHandler
 from fastapi import FastAPI
 from starlette.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+from .plugins.model import Model, DEVICE
 
 BASE_DIR = Path(__file__).resolve().parent
 LOG_FILE_NAME = "FastAPI@Log.txt"
 RELATIVE_ROOT = "./app"
 
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=f"{RELATIVE_ROOT}/static"), name="static")
-
+model = Model(alpha=1e-4).to(DEVICE)
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'pages')))
+
+if os.path.exists(str(Path(BASE_DIR, 'static', 'models', 'model.pth'))):
+    model.load_state_dict(torch.load(str(Path(BASE_DIR, 'static', 'models', 'model.pth')), weights_only=True))
+else:
+    print(str(Path(BASE_DIR, 'static', 'models', 'model.pth')))
+    raise Exception("Train the model first")
 
 if os.path.exists(LOG_FILE_NAME):
     with open(LOG_FILE_NAME, "w") as file:
@@ -20,7 +28,6 @@ if os.path.exists(LOG_FILE_NAME):
 
 if not os.path.exists("./temp"):
     os.makedirs("temp")
-
 
 
 logging.basicConfig( ## settings basic config for logger
--- app/plugins/image_helper.py
+++ app/plugins/image_helper.py
@@ -1,9 +1,8 @@
 import torch
 from torchvision.transforms import v2
 import torchvision.io as io
-from .model import Model, DEVICE
 import os
-from pathlib import Path
+from app import model
 
 RELATIVE_ROOT = "../static/models"
 
@@ -15,16 +14,8 @@ def process_directory(directory):
     transform = v2.Compose([
         v2.Resize(size=(227,227)),
         v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # --> root of error
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # normalizing data with mean of alexnet trained on imagenet
     ])
-    image_tensor = transform(image_tensor) #--> error
+    image_tensor = transform(image_tensor)
     image_tensor = image_tensor.unsqueeze(0)
-    base_dir = Path(__file__).resolve().parent.parent
-    
-    model = Model(alpha=1e-4).to(DEVICE)
-    if os.path.exists(str(Path(base_dir, 'static', 'models', 'model.pth'))):
-        pass
-
-    
-    model.load_state_dict(torch.load(str(Path(base_dir, 'static', 'models', 'model.pth')), weights_only=True))
     return model.prediction(image_tensor)
